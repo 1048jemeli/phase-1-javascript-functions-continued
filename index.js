d83420439460f562b0f9526dca42a39244302149
Nazlin <jemelinazlin@gmail.com>
2024-04-03 08:15:38 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// code your solution here
+// Define the saturdayFun function declaration
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+// Define the mondayWork function expression
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+
+// Define the wrapAdjective function
+function wrapAdjective(flair = "*") {
+    return function(adjective) {
+        return `You are ${flair}${adjective}${flair}!`;
+    }
+}
+
+// Export the functions if needed
+module.exports = {
+    saturdayFun,
+    mondayWork,
+    wrapAdjective
+};
